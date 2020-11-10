f083eb2f4179f551decd5f709e796464494c27d9
Arryos <72743382+Arryos@users.noreply.github.com>
2020-11-10 17:25:40 +0100

    enemy après selection

---
+++ Assets/setActive.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setActive : MonoBehaviour
+{
+    public GameObject gameobject;
+
+    public void Activate()
+    {
+        gameobject.SetActive(true);
+    }
+
+    public void Desactivate()
+    {
+        gameobject.SetActive(false);
+
+    }
+}
