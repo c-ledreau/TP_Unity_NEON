e30ecc6e5d37b1d5030872d1b7cd381d598d5289
c-ledreau <c.ledreau@hotmail.fr>
2020-11-13 15:26:13 +0100

    protextion todo

--- Assets/Scripts/Manager/BossManager.cs
+++ Assets/Scripts/Manager/BossManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BossManager : MonoBehaviour
@@ -12,6 +13,10 @@ public class BossManager : MonoBehaviour
     private Camera m_mainCamera;
     [SerializeField]
     private Player m_player;
+    [SerializeField]
+    private GameObject m_enemyManager;
+    [SerializeField]
+    private TextMeshProUGUI m_alert;
     bool isPoped;
 
     private float height;
@@ -19,6 +24,8 @@ public class BossManager : MonoBehaviour
     void Start()
     {
         isPoped = false;
+        m_alert.gameObject.SetActive(true);
+        m_alert.CrossFadeAlpha(0.0f, 0, false);
     }
 
     // Update is called once per frame
@@ -27,17 +34,33 @@ public class BossManager : MonoBehaviour
         if (m_player.getScore() >= 100 && !isPoped)
         {
             Debug.Log(m_player.getScore());
-            popBoss1();
+            StartCoroutine(PopBoss1());
+            isPoped = true;
         }
     }
 
-    void popBoss1()
+    IEnumerator PopBoss1()
     {
+        //end spawn enemy
+        m_enemyManager.SetActive(false);
+        m_alert.gameObject.SetActive(true);
+        for (int k = 0; k < 2; k++)
+        {
+            m_alert.CrossFadeAlpha(1.0f, 0.5f, false);
+            yield return new WaitForSeconds(1);
+            m_alert.CrossFadeAlpha(0.0f, 0.5f, false);
+            yield return new WaitForSeconds(1);
+        }
+
+        yield return new WaitForSeconds(3);
+        //text appears
+            //spawn boss1
+
+
         boss1.m_mainCamera = m_mainCamera;
         height = m_mainCamera.WorldToScreenPoint(m_spaceship.transform.position).z;
         Vector3 bossPos = m_mainCamera.ScreenToWorldPoint(new Vector3(Random.Range(m_mainCamera.pixelWidth * 0.3f, m_mainCamera.pixelWidth * 0.6f), m_mainCamera.pixelHeight+70, height));
         boss1.transform.position = bossPos;
         Instantiate(boss1);
-        isPoped = true;
     }
 }
